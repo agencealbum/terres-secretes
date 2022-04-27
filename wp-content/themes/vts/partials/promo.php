d14814cdc3a5010694a80f48517bfb285459371a
Sloan Fardèle <sloanfardele@MacBook-Pro-de-Sloan.local>
2022-04-27 09:50:15 +0200

    AEdit on promo NB bandeau

--- wp-content/themes/vts/partials/promo.php
+++ wp-content/themes/vts/partials/promo.php
@@ -10,7 +10,7 @@
 			<div class="col-md-6 px-0 py-0">
                 <img class="promo-img" src="<?=get_the_post_thumbnail_url(33)?>"/>
 			</div>
-			<div class="col-md-6 pb-3 pt-4">
+			<div class="col-md-6 px-5 py-5">
 				<?php echo apply_filters( 'the_content', $post->post_content ); ?>
                 <a target="_blank" class="nuiton-link" href="https://nuiton-beaunoy.fr/">VISITER LE SITE</a>
 			</div>
