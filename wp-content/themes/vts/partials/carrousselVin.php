74f275a4490a7ada6d0208c3acb1155ee49e564c
Sloan Fardèle <sloanfardele@MacBook-Pro-de-Sloan.local>
2022-06-21 11:20:47 +0200

    Fixes for the You may like carousel

--- wp-content/themes/vts/partials/carrousselVin.php
+++ wp-content/themes/vts/partials/carrousselVin.php
@@ -1,25 +1,42 @@
 <?php
 
-$cat = wp_get_post_categories($post->ID);
 
-$vins = new WP_Query(array(
+$cats = get_the_terms(get_the_ID(), 'vins_categories');
+
+$catsSlugs = array();
+
+foreach($cats as $oneCat) {
+    $catsSlugs[] = $oneCat->slug;
+}
+
+$vins = new WP_Query(
+    array(
         'post_type' => 'vins',
-        'vins_categories' => $cat->name
+        'tax_query' => array(
+            array(
+                'taxonomy' => 'vins_categories',
+                'field' => 'slug',
+                'terms' => $catsSlugs,
+            ),
+        ),
+        'post__not_in' => array(get_the_ID())
     )
 );
 
+
+if($vins->found_posts > 0):
 ?>
-<div id="recipeCarousel<?= $cat->term_id; ?>" class="carousel slide w-100 border border-white" data-ride="carousel">
+<div class="carousel slide w-100 border border-white" data-ride="carousel">
     <div class="carousel-inner w-100 py-5 px-2" role="listbox">
 
         <?php $counter = 0;
-        while ($vins->have_posts()) : $vins->the_post(); ?>
+        while($vins->have_posts()) : $vins->the_post(); ?>
             <div class="carousel-item <?= ($counter === 0) ? 'active' : ''; ?>">
                 <div class="col-md-4 text-center">
                     <a href="<?= get_post_permalink(); ?>">
                         <img style="max-height: 40vh; width: auto;" src="<?= the_post_thumbnail_url('medium'); ?>">
                         <div class="vin-block-spe d-block mt-2 <?php
-                        switch (get_field('couleur')):
+                        switch(get_field('couleur')):
                             case "Blanc":
                                 echo "vin-blanc";
                                 break;
@@ -46,11 +63,12 @@ $vins = new WP_Query(array(
             <?php $counter++; endwhile; ?>
     </div>
 </div>
+<?php endif; ?>
 <div class="row" id="nos-vins-link">
     <div class="col-md-4 offset-md-4 justify-content-between align-items-center">
         <a class="border-background btn btn-primary" style="margin-left: 3.2rem"
            href='<?php echo get_post_type_archive_link('vins'); ?>'>
-            <?php if (ICL_LANGUAGE_CODE == "fr"): ?>
+            <?php if(ICL_LANGUAGE_CODE == "fr"): ?>
                 Voir tous nos vins
             <?php else: ?>
                 See all our wines
